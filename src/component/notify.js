15b7cd615da1a9f6a6c5094efc8538d5f136aa5e
Reza Asadi <rezaasadirezaasadi80@gmail.com>
2023-03-20 16:01:57 +0330

    Add notify to show error

---
+++ src/component/notify.js
@@ -0,0 +1,24 @@
+import { toast } from "react-toastify";
+export const notify = (type, me) => {
+  if (type === "s") {
+    toast.success(me, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  } else {
+    toast.error("Invalid data!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+};
