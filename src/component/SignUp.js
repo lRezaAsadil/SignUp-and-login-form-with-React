8e21b26ee7a021f8b9ae66cad952593299c2317d
Reza Asadi <rezaasadirezaasadi80@gmail.com>
2023-03-20 13:45:43 +0330

    Form inputs

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -3,7 +3,33 @@ import React from 'react';
 const SignUp = () => {
     return (
         <div>
-            SignUp
+            <form>
+                <h1>Sign Up</h1>
+                <div>
+                    <label>Name</label>
+                    <input type="text" name='Name'/>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="text" name='Email'/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="Password" name='Password'/>
+                </div>
+                <div>
+                    <label>Confrim Password</label>
+                    <input type="Password" name='ConfrimPassword'/>
+                </div>
+                <div>
+                    <label>I accept terms of privacy policy</label>
+                    <input type="checkbox" name='IsAccepted'/>
+                </div>
+                <div>
+                    <a href="#/">Login</a>
+                    <button type='submit'>Sign up</button>
+                </div>
+            </form>
         </div>
     );
 };
