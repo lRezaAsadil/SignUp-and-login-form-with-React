89abf8a37709d41b245b019bd23cf5480fe0fe6c
Reza Asadi <rezaasadirezaasadi80@gmail.com>
2023-03-20 13:39:00 +0330

    Add signUp to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import SignUp from "./component/SignUp"
 function App() {
   return (
     <div className="App">
-      <h1> Start new from  </h1>
+      <SignUp/>
     </div>
   );
 }
