1dc54378959e35634577d9795feb324b1a496927
Reza Asadi <rezaasadirezaasadi80@gmail.com>
2023-03-20 16:19:53 +0330

    Create Login page

---
+++ src/component/Login.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import validate from "./validate";
+import { notify } from "./notify";
+import styles from "./SignUp.module.css";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const Login = () => {
+  const [focus, setFocus] = useState({});
+  const [errors, setErrors] = useState({});
+  const [data, setData] = useState({
+    Email: "",
+    Password: "",
+  });
+  //   changeHandler
+  const changeHandler = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+  // useEffect
+  useEffect(() => {
+    setErrors(validate(data, "Login"));
+  }, [data, focus]);
+  // focusHandler
+  const focusHandler = (event) => {
+    setFocus({ ...focus, [event.target.name]: true });
+  };
+  //   submitHandler
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!Object.keys(errors).length) {
+      notify("s", "You have logged in");
+    } else {
+      notify("invalid");
+      setFocus({
+        Email: true,
+        Password: true,
+      });
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <form onSubmit={submitHandler} className={styles.formContainer}>
+        <h1 className={styles.header}>Sign Up</h1>
+
+        <div className={styles.formField}>
+          <label>Email</label>
+          <input
+            className={
+              errors.Email && focus.Email
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            type="text"
+            name="Email"
+            value={data.Email}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.Email && focus.Email && <span>{errors.Email}</span>}
+        </div>
+        <div className={styles.formField}>
+          <label>Password</label>
+          <input
+            className={
+              errors.Password && focus.Password
+                ? styles.uncompleted
+                : styles.formInput
+            }
+            type="Password"
+            name="Password"
+            value={data.Password}
+            onChange={changeHandler}
+            onFocus={focusHandler}
+          />
+          {errors.Password && focus.Password && <span>{errors.Password}</span>}
+        </div>
+
+        <div className={styles.formButtons}>
+          <a href="#/">Sign up</a>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
