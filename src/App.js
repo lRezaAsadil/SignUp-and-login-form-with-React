a7701c369ffd77535169550df90a70134e8ba48b
Reza Asadi <rezaasadirezaasadi80@gmail.com>
2023-03-20 16:29:51 +0330

    Use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import Login from './component/Login';
-import SignUp from "./component/SignUp"
+import "./App.css";
+import Login from "./component/Login";
+import SignUp from "./component/SignUp";
+import { Navigate, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <SignUp/>
-      <Login/>
+      <Routes>
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
     </div>
   );
 }
